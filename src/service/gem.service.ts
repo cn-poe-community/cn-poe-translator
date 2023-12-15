3e0aae73aa7b513dee1f5a663d5a2a725651ba11
me1ting <52079867+me1ting@users.noreply.github.com>
2023-12-15 16:45:29 +0800

    feat: remove gem quality types

--- src/service/gem.service.ts
+++ src/service/gem.service.ts
@@ -1,11 +1,5 @@
 import { GemProvider } from "../provider/gem.provider.js";
 
-const QUALITY_TYPES = new Map([
-    ["分歧", "Divergent"],
-    ["异常", "Anomalous"],
-    ["魅影", "Phantasmal"],
-]);
-
 const PROPERTY_NAMES = new Map([
     ["等级", "Level"],
     ["品质", "Quality"],
@@ -15,24 +9,14 @@ export class GemService {
     constructor(private readonly gemProvider: GemProvider) {}
 
     public translateBaseType(zhBaseType: string): string | undefined {
-        zhBaseType = zhBaseType.replace("(", "（").replace(")", "）");
         return this.gemProvider.provideSkill(zhBaseType)?.en;
     }
 
     public translateTypeLine(zhTypeLine: string): string | undefined {
-        let qualityTypePrefix = "";
         let zhSkill = zhTypeLine;
-        for (const [zh, en] of QUALITY_TYPES) {
-            const prefix = `${zh} `;
-            if (zhSkill.startsWith(prefix)) {
-                qualityTypePrefix = `${en} `;
-                zhSkill = zhTypeLine.substring(prefix.length);
-                break;
-            }
-        }
 
         const skill = this.translateBaseType(zhSkill);
-        return skill !== undefined ? `${qualityTypePrefix}${skill}` : undefined;
+        return skill !== undefined ? `${skill}` : undefined;
     }
 
     public translatePropertyName(zhName: string): string | undefined {
