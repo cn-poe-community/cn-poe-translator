2a8f77a0dc4072831edbed6cde71c7795657bbac
me1ting <52079867+me1ting@users.noreply.github.com>
2024-01-03 15:07:28 +0800

    Update version

--- src/index.global.ts
+++ src/index.global.ts
@@ -1,10 +1,17 @@
 import { Assets } from "cn-poe-export-db/dist/types.js";
 import { BasicTranslatorFactory } from "./index.js";
 
+/**
+ * 
+ * @param assets 
+ * @returns 
+ * @deprecated replaced by BasicTranslatorFactory
+ */
 function newBasicTranslatorFactory(assets: Assets) {
     return new BasicTranslatorFactory(assets);
 }
 
 export default {
     newBasicTranslatorFactory,
+    BasicTranslatorFactory,
 };
